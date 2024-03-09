7c1a980807159523695f7222afb3813c1a776a39
a0kribu <kribu@spire.ee>
2024-03-09 17:13:57 +0200

    building the start

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+def main():
+    node = TextNode("15", "txt", "https://spire.ee")
+    print(node)
+
+
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,18 @@
+class TextNode():
+    def __init__(self, text="", text_type="", url=""):
+        self.text=text
+        self.text_type=text_type
+        self.url=url
+    
+    
+
+    
+    def __eq__(self, another):
+        return (self.text==another.text and self.text_type==another.text_type and self.url==another.url)
+    
+
+    def __repr__(self):
+        return f'TextNode: {self.text}, {self.text_type}, {self.url}'
+    
+
+    
